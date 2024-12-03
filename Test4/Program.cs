f29a94d81dc863107a6a67e72417441262cf1fb2
Nele Oks <nele.oks@tktk.ee>
2024-12-03 19:39:30 +0200

    add Week4Loops code

---
+++ Test4/Program.cs
@@ -0,0 +1,29 @@
+﻿//string userPIN;
+//int triesUsed = 0;
+//while (triesUsed != 3)
+//{
+// Console.WriteLine("Enter your PIN");
+//  userPIN = Console.ReadLine();
+//  if (userPIN == "1234")
+//  {
+//      Console.WriteLine("Welcome!");
+//      triesUsed = 3; //number
+//  }
+//   else
+//  {
+//      Console.WriteLine($"Wrong PIN.Try again. {3 - triesUsed} tries left.");
+//       triesUsed++;
+//   }
+//}
+
+//for(int i = 1; i< 5; i++)
+//{
+//   Console.WriteLine("Hello, World!");
+//}
+
+Console.WriteLine("Enter a number");
+int userNumber + Int32.Parse(Console.Readline());
+for (int i = 0 ; i < 10 ; i++)
+{
+    Console.WriteLine($"{i +  1} * {userNumber} = {(i + 1) * userNumber}");
+}
